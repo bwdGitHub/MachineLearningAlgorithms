5fcda515228641d1720e91d128d91915a7453113
Ben Wells-Day <benwellsday@gmail.com>
2018-02-28 14:38:10 +0000

    Added numpy import to mathematics.

--- mathematics.py
+++ mathematics.py
@@ -1,6 +1,6 @@
 # Mathematics helper functions.
 # Note many of these are possibly defined in numpy already.
-
+import numpy as np
 # Simple Lp metric.
 def lp_metric(x,y, p=2.0):
     return np.power(np.sum(np.power(np.absolute(x-y), p)),1/p)
