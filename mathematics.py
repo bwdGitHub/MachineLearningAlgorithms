ae54a1a489f5c67a0d645fe46b7eba5576067377
Ben Wells-Day <benwellsday@gmail.com>
2018-02-28 14:24:34 +0000

    Added a Gaussian kernel to mathematics.py

--- mathematics.py
+++ mathematics.py
@@ -5,3 +5,8 @@ def lp_metric(x,y, p=2.0):
     return np.power(np.sum(np.power(np.absolute(x-y), p)),1/p)
 def euclidean_metric(x,y):
     return lp_metric(x,y,2)
+
+# A gaussian kernel on Lp metrics.
+def gaussian_kernel(x,y,sigma, p=2.0):
+    dist = lp_metric(x,y, p=p)
+    return np.exp(-(dist**2)/(2*sigma**2))
