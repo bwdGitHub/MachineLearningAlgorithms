Fixed class-conditional std to return the covariance matrix.

classification.py
@@ -55,6 +55,32 @@ def class_conditional_mean(X,y):
 def class_conditional_std(X,y):
     unique, counts = np.unique(y, return_counts = True)
     stds = {}
+    means = class_conditional_mean(X,y)
     for yi, count in zip(unique, counts):
-        stds[yi] = np.std(X[y==yi], axis = 0)
+        Xs = X[y ==yi]
+        outers = np.zeros(np.outer(Xs[0],Xs[0]).shape)
+        for x in Xs:
+            outers += np.outer(x-means[yi], x-means[yi])
+        stds[yi] = outers/count
     return stds
+
+# We can use the above to find the probabilities that a point is in a particular class, assuming these are Gaussian.
+def gaussian2(x,m,s):
+    exponent = (np.sum(np.power(x-m,2))/(2*(s**2)))
+    denominator = s*np.sqrt(2*np.pi)
+    return np.exp(-exponent)/denominator
+def plug_in_probability(x, X,y):
+    prior = priors(y)
+    means = class_conditional_mean(X,y)
+    stds = class_conditional_std(X,y)
+    prob = {}
+    total_prob = 0
+    from mathematics import gaussian
+    for clf in prior:
+        prob[clf] = prior[clf]*gaussian2(x,means[clf],stds[clf])
+        #gaussian(x, means[clf], stds[clf])*prior[clf]
+        total_prob+=prob[clf]
+    # normalise the probabilites
+    for clf in prob:
+        prob[clf] /= total_prob
+    return prob